61bd3249ae2a0eadc406405ff6064871d73dd6e0
electrosy <heysteve5@gmail.com>
2020-02-15 08:55:41 -0700

    added a block class

---
+++ Block.cpp
@@ -0,0 +1,18 @@
+/* 
+Author: Steven Philley
+Purpose: see header.
+Date: Feb/15/2020
+*/
+#include "Block.h"
+
+/* RAII */
+ley::Block::Block() {
+    
+    
+}
+
+ley::Block::~Block() {
+
+}
+
+/* Functions */
---
+++ Block.h
@@ -0,0 +1,22 @@
+/* 
+Author: Steven Philley
+Purpose: A Block is a collection of sprites either animated or constructed together to make a larger sprite.
+Date: Feb/15/2020
+*/
+
+#ifndef BLOCK_H
+#define BLOCK_H
+
+namespace ley {
+
+class Block {
+
+private:
+    
+public:
+    Block();
+    ~Block();
+};
+
+}
+#endif
