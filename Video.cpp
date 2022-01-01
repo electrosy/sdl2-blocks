802a3b6f051d4c6aefcfc29f1fe96745d19617f5
electrosy <heysteve5@gmail.com>
2022-01-01 15:57:46 -0700

    Version 0.1.7.7

--- Video.cpp
+++ Video.cpp
@@ -19,7 +19,7 @@ const auto SCREEN_WIDTH = 1280;
 const auto SCREEN_HEIGHT = 720;
 
 const std::string APPLICATION_NAME = "Ablockalypse";
-const std::string APPLICATION_VER = "0.1.7.6";
+const std::string APPLICATION_VER = "0.1.7.7";
 const std::string APPLICATION_PLATFORM = SDL_GetPlatform();
 const std::string APPLICATION_REL_TYPE = "Alpha";
 const std::string APPLICATION_ENV = "Development";
