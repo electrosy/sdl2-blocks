6976a027014d8954867085dc04d7ae6a27668892
electrosy <heysteve5@gmail.com>
2024-03-01 19:47:58 -0800

    Use random device for Linux and current time for windows.

--- Rand_int.cpp
+++ Rand_int.cpp
@@ -17,8 +17,16 @@ dis{low,high} {
 	SDL_Log("Random device entropy %f", rd.entropy());
 
 	if(rd.entropy() == 0) {
+		// https://stackoverflow.com/questions/12469171/code-to-generate-random-numbers-in-c
+		std::chrono::time_point<std::chrono::system_clock>now {std::chrono::system_clock::now()};
+		std::chrono::system_clock::duration epoch {now.time_since_epoch()};
+		typedef std::chrono::duration<unsigned long long int, std::milli> ms;
+
+		auto seed = std::chrono::duration_cast<ms>(epoch).count() + SDL_GetTicks();
+
 		SDL_Log("Setting random generator from rd() to SDL_GetTicks()");
-		gen.seed(SDL_GetTicks());
+		gen.seed(seed);
+		SDL_Log("std::chrono::duration_cast<ms>(epoch).count() + SDL_GetTicks() value: %d", seed);
 	}
 }
 
