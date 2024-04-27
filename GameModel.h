674dbefe2468d7ac1db068807529b6b4e280677b
electrosy <heysteve5@gmail.com>
2024-04-26 20:37:34 -0700

    Move board size constants into the board object.

--- GameModel.h
+++ GameModel.h
@@ -16,7 +16,6 @@ Date: Feb/15/2020
 #include "Timer.h"
 #include "Input.h"
 #include "HighScores.h"
-#include "./inc/Screen.h"
 #include "inc/Config.h"
 #include "inc/Board.h"
 
@@ -37,51 +36,6 @@ enum class UIFocusChange {
     goto_game,
     game
 };
-
-
-const auto BOARDSIZE_BUFFER = 3; // buffer above board for piece to come into play
-const auto BLOCKSIZE_PX = 30; // blocksize in pixels
-
-const auto BOARD_POS_Y_PX = 40; //Board starting position y for block, when drawing border subtract 1px.
-const auto BLOCK_START_POS_Y_PX = BOARD_POS_Y_PX - (BLOCKSIZE_PX*BOARDSIZE_BUFFER); //Subtract three rows from board start_y //Number of pixels to offset the board.
-
-const auto NEXTBOX_PADDING_PX = 25; // padding from the game board
-const auto NEXTBOX_SIZE_PX = (BLOCKSIZE_PX * 4) + 6; // four blocks can fit in the next border plus 5px padding.
-const auto NEXTBOX_POS_Y_PX = BOARD_POS_Y_PX;
-
-const auto NEXTBLOCK_OFFSET_Y_PX = 10;
-const auto NEW_LVL_AT_LINES = 10;
-auto const COMBO_POS_Y_PX = NEXTBOX_POS_Y_PX + NEXTBOX_SIZE_PX + 40;
-
-auto const LINES_POS_Y_PX = 40;
-auto const LVL_POS_Y_PX = 65;
-auto const SCORE_POS_Y_PX = 90;
-
-const auto PTS_DROP = 5;
-const auto PTS_2LINE_MULT = 5;
-const auto PTS_3LINE_MULT = 15;
-const auto PTS_4LINE_MULT = 45;
-
-
-//changes based on board width and height
-const auto BOARDSIZE_WIDTH = 10; // TODO ADD FEATURE - this should be the first option added.
-const auto BOARDSIZE_WIDTH_PX = BOARDSIZE_WIDTH * BLOCKSIZE_PX;
-const auto BOARD_POS_X_PX =  (SCREEN_WIDTH / 2) - (BOARDSIZE_WIDTH_PX / 2);//490; //Board starting position x for block, when drawing border subtract 1px.
-auto const LINES_POS_X_PX = BOARD_POS_X_PX + BOARDSIZE_WIDTH_PX + NEXTBOX_PADDING_PX; //Lines text
-auto const LVL_POS_X_PX = BOARD_POS_X_PX + BOARDSIZE_WIDTH_PX + NEXTBOX_PADDING_PX; //Level text
-auto const SCORE_POS_X_PX = BOARD_POS_X_PX + BOARDSIZE_WIDTH_PX + NEXTBOX_PADDING_PX; //Score text;
-const auto PTS_LINE = BOARDSIZE_WIDTH * (PTS_DROP * 2); // x Level
-
-const auto BLOCK_START_POS_X_PX = BOARD_POS_X_PX;
-const auto NEXTBOX_POS_X_PX = BOARD_POS_X_PX - NEXTBOX_PADDING_PX - NEXTBOX_SIZE_PX;
-
-const auto NEXTBLOCK_OFFSET_X_PX = BLOCK_START_POS_X_PX - 130;
-auto const COMBO_POS_X_PX = NEXTBOX_POS_X_PX;
-
-//changes based on board height
-const auto BOARDSIZE_HEIGHT = 20 + BOARDSIZE_BUFFER; //add 3 additional blocks so tetrominos can enter play in the right place.
-const auto BOARDSIZE_HEIGHT_PX = (BOARDSIZE_HEIGHT - BOARDSIZE_BUFFER) * BLOCKSIZE_PX;
-
 
 
 class GameModel {
