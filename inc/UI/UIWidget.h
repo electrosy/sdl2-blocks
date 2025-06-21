be25fea314d17664d57b16479e85c5748e7e388d
electrosy <heysteve5@gmail.com>
2025-06-20 20:20:08 -0700

    a bit more refactoring

--- inc/UI/UIWidget.h
+++ inc/UI/UIWidget.h
@@ -18,29 +18,28 @@ public:
     virtual void render(SDL_Renderer* r, bool d) = 0;
     virtual void update() = 0; //handles update part of the lifecycle.
     virtual void processInput(std::string s) = 0;
-    virtual std::string getTextBoxValue() = 0;
-    virtual void setTextBoxValue(std::string s) = 0;
-    virtual std::string* getTextBoxField() = 0;
+    virtual void handleFocusChange(UIWidget** activeUIElement, std::string* previousValue) = 0;
     virtual void toggleFocus() = 0;
     virtual bool hasFocus()  = 0;
-    virtual void setPos(SDL_Point p) = 0;
     virtual void onKeyDown(ley::Character c) = 0;
     virtual void onTextInput(const char* cstr) = 0;
-    virtual void setCharSound(const std::function<void()> &func) = 0;
-    virtual void setBackspaceSound(const std::function<void()> &func) = 0;
     virtual std::string getHelpMessage() = 0;
-    virtual void setWidth(int width, int underlineWidth, int maxCharLength) = 0;
     virtual ley::Font* getErrorFontPtr()  = 0;
     virtual ley::Font* getHelpFontPtr()  = 0;
     virtual std::string getRegEx() = 0;
-    virtual void setRegEx(std::string regEx)  = 0;
     virtual ley::Timer* getErrorTimerPtr() = 0;
+    virtual std::string* getTextBoxField() = 0;
+    virtual std::string getTextBoxValue() = 0;
+    virtual void setPos(SDL_Point p) = 0;
+    virtual void setCharSound(const std::function<void()> &func) = 0;
+    virtual void setBackspaceSound(const std::function<void()> &func) = 0;
     virtual void setHelpMessages(std::string focusHelp, std::string nonFocusHelp) = 0;
     virtual void setErrorMessage(std::string errorMessage) = 0;
-    virtual void handleFocusChange(UIWidget** activeUIElement, std::string* previousValue) = 0;
+    virtual void setTextBoxValue(std::string s) = 0;
+    virtual void setRegEx(std::string regEx)  = 0;
+    virtual void setWidth(int width, int underlineWidth, int maxCharLength) = 0;
     
-    std::string getLastChar(); 
-
+    std::string getLastChar(); // The most recently captured character from SDL_TEXTINPUT
 };
 
 }
